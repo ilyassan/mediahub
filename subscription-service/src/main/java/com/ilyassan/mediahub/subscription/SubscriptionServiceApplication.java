a8827cc31e72844b3e4057f020f507b774823929
ilyass anida <anidailyass@gmail.com>
2026-03-03 10:06:45 +0000

    feat: restructure project as multi-module Maven microservices architecture

--- subscription-service/src/main/java/com/ilyassan/mediahub/subscription/SubscriptionServiceApplication.java
+++ subscription-service/src/main/java/com/ilyassan/mediahub/subscription/SubscriptionServiceApplication.java
@@ -1,13 +1,12 @@
-package com.ilyassan.multimedia;
+package com.ilyassan.mediahub.subscription;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MultimediaApplication {
+public class SubscriptionServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MultimediaApplication.class, args);
+        SpringApplication.run(SubscriptionServiceApplication.class, args);
     }
-
 }
