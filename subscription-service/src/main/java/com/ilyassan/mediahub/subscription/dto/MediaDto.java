09b5fc8235ee04e5cbc7c62febbe8220770a2f25
Mohamed Hamouch <med10hamouch@gmail.com>
2026-03-11 11:17:42 +0000

    add subscription and media DTOs

--- subscription-service/src/main/java/com/ilyassan/mediahub/subscription/dto/MediaDto.java
+++ subscription-service/src/main/java/com/ilyassan/mediahub/subscription/dto/MediaDto.java
@@ -2,10 +2,9 @@ package com.ilyassan.mediahub.subscription.dto;
 
 import lombok.Data;
 
-// Mirrors the MediaDto returned by media-service.
-// Used by the Feign client to deserialize the response JSON.
 @Data
 public class MediaDto {
+
     private Long id;
     private String title;
     private String type;
