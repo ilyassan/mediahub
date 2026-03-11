b635829a38c532521c99f0ed8455671e1f59cd40
Mohamed Hamouch <med10hamouch@gmail.com>
2026-03-11 12:08:15 +0000

    add description to MediaDto and add MediaResponseDto wrapper

--- subscription-service/src/main/java/com/ilyassan/mediahub/subscription/dto/MediaDto.java
+++ subscription-service/src/main/java/com/ilyassan/mediahub/subscription/dto/MediaDto.java
@@ -7,6 +7,7 @@ public class MediaDto {
 
     private Long id;
     private String title;
+    private String description;
     private String type;
     private String genre;
     private String category;
